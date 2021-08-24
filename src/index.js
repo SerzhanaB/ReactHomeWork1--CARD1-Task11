228d8431dabe915aa393dcd980165a27143c5c57
Serzhana <buraevase@gmail.com>
2021-08-25 00:21:23 +0300

    1 коммит

--- src/index.js
+++ src/index.js
@@ -3,14 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
+import "antd/dist/antd.css";
+import { Input } from 'antd';
+ReactDOM.render(<Input placeholder="Basic usage" />, mountNode);
+<div>ЧТо-то пока не получается добавить элемент из библиотеки</div>
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
